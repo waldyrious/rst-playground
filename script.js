6f9b90555a9ff0ddbbac1f66129209d3dfb779ed
Waldir Pimenta <waldyrious@gmail.com>
2025-11-20 23:07:55 +0000

    Check for WebAssembly only once

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const inputTextarea = document.getElementById("rst-input");
 const outputFrame = document.getElementById("html-output");
 
+// Check if the browser supports WebAssembly
+if (!("WebAssembly" in window)) {
+  const pre = document.createElement("pre");
+  pre.textContent = "This website requires WebAssembly because it depends on Pyodide to run docutils in the browser";
+  outputFrame.srcdoc = pre.outerHTML;
+}
+
 // Activate controls that are inert if JavaScript is disabled
 inputTextarea.disabled = false;
 inputTextarea.placeholder = "Enter reStructuredText content here.";
 outputFrame.contentDocument.write("<!DOCTYPE html> Initializing...\n");
-
-// Check if the browser supports WebAssembly
-function checkForWebAssembly() {
-  if (!("WebAssembly" in window)) {
-    throw new Error("This website requires WebAssembly because it depends on Pyodide to run docutils in the browser");
-  }
-}
 
 // Initialize Pyodide
 async function main() {
@@ -25,12 +25,12 @@ async function main() {
   // This makes the browser favicon stop the loading spinner
   outputFrame.contentDocument.close();
 
-  // Trigger rendering whenever the textarea content changes (typing, pasting, etc.)
-  inputTextarea.addEventListener("input", debouncedConvert);
-
   return pyodide;
 }
 let pyodideReadyPromise = main();
+
+// Trigger rendering whenever the textarea content changes (typing, pasting, etc.)
+inputTextarea.addEventListener("input", debouncedConvert);
 
 // Define the conversion delay timer outside the delaying function itself
 // so it can be reused (i.e. reset) across function calls.
@@ -50,37 +50,30 @@ function debouncedConvert() {
 }
 
 async function rstToHtml() {
-  try {
-    checkForWebAssembly();
-    let pyodide = await pyodideReadyPromise;
+  let pyodide = await pyodideReadyPromise;
 
-    // We pass the textarea contents as a variable
-    // instead of interpolating them into the code below.
-    // For the reasoning, see the description of commit 40037ef37.
-    pyodide.globals.set("input_text", inputTextarea.value);
+  // We pass the textarea contents as a variable
+  // instead of interpolating them into the code below.
+  // For the reasoning, see the description of commit 40037ef37.
+  pyodide.globals.set("input_text", inputTextarea.value);
 
-    // Python code to parse a rST string into HTML using docutils
-    // See: https://docutils.sourceforge.io/docs/api/publisher.html#publish-string
-    // Note: the `decode()` is needed to convert `publish_string()`'s output
-    // from a bytestring to a plain string. See https://stackoverflow.com/a/606199/266309.
-    let htmlString = await pyodide.runPythonAsync(`
-      from docutils.core import publish_string
-      publish_string(input_text, writer_name="html5").decode("utf-8")
-    `);
+  // Python code to parse a rST string into HTML using docutils
+  // See: https://docutils.sourceforge.io/docs/api/publisher.html#publish-string
+  // Note: the `decode()` is needed to convert `publish_string()`'s output
+  // from a bytestring to a plain string. See https://stackoverflow.com/a/606199/266309.
+  let htmlString = await pyodide.runPythonAsync(`
+    from docutils.core import publish_string
+    publish_string(input_text, writer_name="html5").decode("utf-8")
+  `);
 
-    // Modify the output HTML string to include the docutils-iframe.css file.
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(htmlString, "text/html");
-    const linkTag = doc.createElement("link");
-    linkTag.rel = "stylesheet";
-    linkTag.href = "css/docutils-iframe.css";
-    doc.head.appendChild(linkTag);
-    htmlString = "<!DOCTYPE html>\n" + doc.documentElement.outerHTML;
+  // Modify the output HTML string to include the docutils-iframe.css file.
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, "text/html");
+  const linkTag = doc.createElement("link");
+  linkTag.rel = "stylesheet";
+  linkTag.href = "css/docutils-iframe.css";
+  doc.head.appendChild(linkTag);
+  htmlString = "<!DOCTYPE html>\n" + doc.documentElement.outerHTML;
 
-    outputFrame.srcdoc = htmlString;
-  } catch (err) {
-    const pre = document.createElement("pre");
-    pre.textContent = err;
-    outputFrame.srcdoc = pre.outerHTML;
-  }
+  outputFrame.srcdoc = htmlString;
 }
